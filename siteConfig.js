2da0b84a2f322c8c2bbc8d46c266922294f84c86
sjvasconcello <sjvasconcello@gmail.com>
2020-03-17 00:08:19 -0300

    siteCongif.js added

---
+++ siteConfig.js
@@ -0,0 +1,6 @@
+const siteConfig = {
+  url: "https://sjvasconcello.github.io", // Your website URL
+  baseUrl: "/ICS733_documentation/",
+  projectName: "ICS733_documentation",
+  organizationName: "sjvasconcello"
+};
